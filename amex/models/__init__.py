12c021c681f607f10739c7b2266d8dde2794f9fa
Najib Ishaq <najib_ishaq@zoho.com>
2022-07-30 11:54:50 -0400

    wip: using pytorch-tabnet instead

--- amex/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import tf_tabnet as tf_tabnet
--- amex/models/tf_tabnet/__init__.py
+++
@@ -1,6 +0,0 @@
-""" https://www.kaggle.com/code/hinepo/amex-tabnet-training
-"""
-
-from . import metrics as metrics
-from .tf_tabnet import read_data
-from .tf_tabnet import run_training
